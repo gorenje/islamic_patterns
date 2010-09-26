4e35bb8cca7c9b4494f4b8988e29508d96919c11
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-09-26 13:59:08 +0200

    added processing to repo. Completed first version of the controls

--- controls.js
+++ controls.js
@@ -1,4 +1,9 @@
 function ctrl_redraw() {
+  var cnvs_container = document.getElementById('canvas_container');
+  var pattern_num = document.getElementById('pattern_selection').value;
+  cnvs_container.innerHTML = '<canvas id="the_canvas" width="600" height="600" datasrc="pattern_' + pattern_num + '.pjs"></canvas>';
+  Processing.reload();
+  
   var cnvs = Processing.getInstanceById('the_canvas');
   cnvs.setup();
   cnvs.loop();
