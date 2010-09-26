330789cd568014ac93707cf5950c67af255c12e7
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-09-26 14:12:58 +0200

    fixes

--- controls.js
+++ controls.js
@@ -1,8 +1,12 @@
 function ctrl_redraw() {
   var cnvs_container = document.getElementById('canvas_container');
-  var pattern_num = document.getElementById('pattern_selection').value;
-  cnvs_container.innerHTML = '<canvas id="the_canvas" width="600" height="600" datasrc="pattern_' + pattern_num + '.pjs"></canvas>';
-  Processing.reload();
+  var datasrc = "pattern_" + document.getElementById('pattern_selection').value + ".pjs";
+  var regexp = new RegExp(datasrc);
+
+  if ( !regexp.test(cnvs_container.innerHTML) ) {
+    cnvs_container.innerHTML = '<canvas id="the_canvas" datasrc="'+datasrc+'"></canvas>';
+    Processing.reload();
+  }
   
   var cnvs = Processing.getInstanceById('the_canvas');
   cnvs.setup();
